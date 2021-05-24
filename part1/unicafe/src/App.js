a12e0454a324b437b9876da7889379d1be1dddb7
Asem <alghabra.asem@gmail.com>
2021-05-24 06:04:07 +0200

    1.10 +  refactoring

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,57 +6,54 @@ import { React, useState } from 'react'
 const Button = ({ text, eventHandler }) => <button onClick={eventHandler}>{text}</button>
 
 // Statisitic Component
-const Statisitic = ({ text, stat }) => <li >{text} {stat}</li>
+const Statisitic = ({ text, value }) => (
+    <tr>
+        <td>{text}</td>
+        <td> {value}</td>
+    </tr>);
 
-// App Component
+// App 
 const App = () => {
     //App States:
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-
+    const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 })
+    const { good, bad, neutral } = feedback
     const all = good + bad + neutral
 
     // Eventhandlers Functions:
     // good
-    const handleGoodClick = () => setGood(good + 1)
+    const handleGoodClick = () => setFeedback({ ...feedback, good: good + 1 })
     // neutral
-    const handleNeutralClick = () => setNeutral(neutral + 1)
+    const handleNeutralClick = () => setFeedback({ ...feedback, neutral: neutral + 1 })
     // bad
-    const handleBadClick = () => setBad(bad + 1)
-
-
-    if (good || bad || neutral) {
-        return (
-            <div>
-                <h1>give feedback</h1>
-                <Button text='good' eventHandler={handleGoodClick} />
-                <Button text='neutral' eventHandler={handleNeutralClick} />
-                <Button text='bad' eventHandler={handleBadClick} />
-                <h1>statistics</h1>
-                <ul>
-
-                    <Statisitic text='good' stat={good} />
-                    <Statisitic text='neutral' stat={neutral} />
-                    <Statisitic text='bad' stat={bad} />
-                    <Statisitic text='all' stat={all} />
-                    <Statisitic text='average' stat={(good - bad) / all} />
-                    <Statisitic text='positive' stat={good / all * 100 + ' %'} />
-                </ul>
-
-            </div>
-        )
-    } else return (
+    const handleBadClick = () => setFeedback({ ...feedback, bad: bad + 1 })
+    // Condition rendering
+    const Statisitics = () => {
+        if (good || bad || neutral) {
+            return (
+                <table>
+                    <tbody>
+                        <Statisitic text='good' value={good} />
+                        <Statisitic text='neutral' value={neutral} />
+                        <Statisitic text='bad' value={bad} />
+                        <Statisitic text='all' value={all} />
+                        <Statisitic text='average' value={((good - bad) / all).toFixed(1)} />
+                        <Statisitic text='positive' value={(good / all * 100).toFixed(1) + ' %'} />
+                    </tbody>
+                </table>)
+        }
+        return <p>No feedback given</p>
+    }
+    return (
         <div>
             <h1>give feedback</h1>
             <Button text='good' eventHandler={handleGoodClick} />
             <Button text='neutral' eventHandler={handleNeutralClick} />
             <Button text='bad' eventHandler={handleBadClick} />
             <h1>statistics</h1>
-            <ul>
-                <li>No feedback given</li>
-            </ul>
-        </div>)
+            <Statisitics />
+        </div>
+    )
+
 }
 
 export default App
