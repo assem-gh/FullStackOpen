a495e688326ba9786cf9abf242eb4ce21c5b1ac2
Asem <alghabra.asem@gmail.com>
2021-06-02 08:02:58 +0200

    3.1

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
-const http = require('http')
+const { request, response } = require('express')
+const express = require('express')
+const app = express()
 
-let data = [
+let personsData = [
     {
         "name": "Arto Hellas",
         "number": "040-123456",
@@ -22,10 +24,14 @@ let data = [
         "id": 4
     }
 ]
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(data))
+app.get('/', (request, response) => {
+    response.send('<h1>Hello</h1>')
 })
-const PORT = 3003
-app.listen(PORT)
-console.log('server running on ', PORT)
+app.get('/api/persons', (request, response) => {
+    response.json(personsData)
+})
+const PORT = 3001
+app.listen((PORT), () => {
+    console.log('server running on ', PORT)
+})
+
