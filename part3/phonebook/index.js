b1db5abf72fd531c617a8b87c7e134367c4b680a
Asem <alghabra.asem@gmail.com>
2021-07-02 12:06:57 +0200

    3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,7 +5,12 @@ const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('personData', (request, response) => JSON.stringify(request.body))
+
+app.use(
+    morgan(':method :url :status :res[content-length] - :response-time ms :personData')
+)
 
 let personsData = [
     {
@@ -37,6 +42,7 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
+    console.log(request)
     response.json(personsData)
 })
 
